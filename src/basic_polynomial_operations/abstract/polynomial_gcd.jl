d1b3fec74d04e0e5e5ea01b29a071bc4dc1fe432
durkinsam <s.durkin@student.uq.edu.au>
2025-10-07 12:40:57 +1000

    Update polynomial_gcd.jl

--- src/basic_polynomial_operations/abstract/polynomial_gcd.jl
+++ src/basic_polynomial_operations/abstract/polynomial_gcd.jl
@@ -8,12 +8,8 @@
 
 """
 The extended euclid algorithm for polynomials (of the same concrete subtype) modulo prime.
-
-Note, when working with polynomials over Z[x] we cannot compute the extended euclidean algorithm (EEA) directly.
-This is because we do not have division in Z[x]. Hence, we must drop to the field Zp[x] and compute the EEA
-with respect to some prime. 
 """
-function extended_euclid_alg_mod_p(a::P, b::P, prime::Integer) where {P <: Polynomial}
+function extended_euclid_alg_mod_p(a::P, b::P, prime::Integer) where {C,D,P<:Polynomial{C,D}}
     old_r, r = mod(a, prime), mod(b, prime)
     old_s, s = one(P), zero(P)
     old_t, t = zero(P), one(P)
@@ -27,10 +23,11 @@ function extended_euclid_alg_mod_p(a::P, b::P, prime::Integer) where {P <: Polyn
     g, s, t = old_r, old_s, old_t
 
     @assert mod(s*a + t*b - g, prime) == 0
-    return g, s, t  
+    return g, s, t
 end
 
 """
 The GCD of two polynomials (of the same concrete subtype) modulo prime.
 """
-gcd_mod_p(a::P, b::P, prime::Integer) where {P <: Polynomial} = extended_euclid_alg_mod_p(a, b, prime) |> first
+gcd_mod_p(a::P, b::P, prime::Integer) where {C,D,P<:Polynomial{C,D}} =
+    extended_euclid_alg_mod_p(a, b, prime) |> first
