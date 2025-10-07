4d348f27e24a93ffff8a7c7cdd0a2c2a8231c958
durkinsam <s.durkin@student.uq.edu.au>
2025-10-07 13:58:36 +1000

    Update polynomial_multiplication.jl

--- src/basic_polynomial_operations/abstract/polynomial_multiplication.jl
+++ src/basic_polynomial_operations/abstract/polynomial_multiplication.jl
@@ -19,15 +19,43 @@ function *(p1::P, p2::P)::P where {C,D,P<:Polynomial{C,D}}
 end
 
 """
-Power of a polynomial.
+Power of a polynomial (fast exponentiation by squaring). Task 4
 """
-function ^(p::P, n::Integer)::P where {C,D,P<:Polynomial{C,D}}
-    n < 0 && error("No negative power")
-    out = one(p)
-    for _ in 1:n
-        out *= p
+function Base.:^(p::P, n::Integer)::P where {C,D,P<:Polynomial{C,D}}
+    n < 0 && throw(ArgumentError("No negative power"))
+
+    # O(1) special cases: 0^n, 1^n, x^n
+    if n == 0
+        return one(P)
+    elseif iszero(p)
+        return zero(P)
+    elseif degree(p) == zero(D) && leading(p).coeff == one(C)
+        return one(P)
+    else
+        it = iterate(p)
+        if it !== nothing
+            t, st = it
+            if t.coeff == one(C) && t.degree == one(D) && iterate(p, st) === nothing
+                return P([Term{C,D}(one(C), convert(D,n))])  # x^n
+            end
+        end
     end
-    return out
+
+    # repeated squaring
+    res  = one(P)
+    base = p
+    e = n
+    while e > 0
+        if (e & 1) == 1
+            res = res * base
+        end
+        e >>= 1
+        if e > 0
+            base = base * base
+        end
+    end
+    return res
 end
+
 
 
